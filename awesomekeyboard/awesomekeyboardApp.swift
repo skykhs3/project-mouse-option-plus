86832c5f89ee32fe5d5f6aaaf115b1399b6eda3b
skykhs3 <skykhs3@naver.com>
2025-10-12 23:34:34 +0900

    Initial Commit

---
+++ awesomekeyboard/awesomekeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  awesomekeyboardApp.swift
+//  awesomekeyboard
+//
+//  Created by USER on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct awesomekeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ awesomekeyboardTests/awesomekeyboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  awesomekeyboardTests.swift
+//  awesomekeyboardTests
+//
+//  Created by USER on 10/12/25.
+//
+
+import Testing
+@testable import awesomekeyboard
+
+struct awesomekeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
